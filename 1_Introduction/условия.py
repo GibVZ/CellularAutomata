50630cd99233e9dc3943feeed40f2ad4d9b78ffa
GibVZ <78082142+GibVZ@users.noreply.github.com>
2026-03-10 22:34:39 +0300

    Add files via upload
    supportive files for remember basics of Python

---
+++ 1_Introduction/условия.py
@@ -0,0 +1,16 @@
+## Условия
+
+##if condition:
+##    do
+##elif other_condition:
+##    do
+##else:
+##    do
+
+a = 10
+if a < 5:
+    print('a is small')
+elif a == 5:
+    print('a is 5')
+else:
+    print('a is big')
